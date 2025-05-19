8c47808a93f614f18e3f2e6c674dd34f678adf04
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2025-05-19 21:38:57 +0530

    updates

--- src/Config/Firebase.js
+++ src/Config/Firebase.js
@@ -6,12 +6,12 @@ import{getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFKTIVHFx5gEPgxUYPDYMBIr5SonrYMd8",
-  authDomain: "todo-list-1b5ab.firebaseapp.com",
-  projectId: "todo-list-1b5ab",
-  storageBucket: "todo-list-1b5ab.firebasestorage.app",
-  messagingSenderId: "733533401324",
-  appId: "1:733533401324:web:c8fe1d29dc04af338e35c9"
+ apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
